cf6d513dd25238519ae2a0fc40e6f031fe63f018
Miguel Lopes Santos Ramos <mail@miguel.ramos.name>
2013-04-19 13:17:48 +0100

    Simpler main module.

--- haskell/fibonacci/fibo.hs
+++ haskell/fibonacci/fibo.hs
@@ -25,16 +25,3 @@ main = do
       putStrLn $ show $ L.fib n
     [ "list", arg1, arg2 ] | i <- read arg1, f <- read arg2 ->
       putStrLn $ show $ take f $ drop i $ L.fibs
-    [ "test", arg ] | n <- read arg ->
-      putStrLn $ show $
-        let a = N.fib n
-	    t = T.fib n
-	    l = L.fib n
-	in a == t && a == l
-    [ "test", arg1, arg2 ] | i <- read arg1, f <- read arg2 ->
-      putStrLn $ show $
-        let a = take f $ drop i $ fibs N.fib
-	    t = take f $ drop i $ fibs T.fib
-	    l = take f $ drop i $ L.fibs
-	    equal3 a b c = a == b && a == c
-	in and $ zipWith3 equal3 a t l
