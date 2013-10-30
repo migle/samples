9a04c0c2cc5d72d48f2fcf1903c9a1ef4fd7da85
Miguel Lopes Santos Ramos <mail@miguel.ramos.name>
2013-10-30 14:38:17 +0000

    Static Matrix Eq and Show.

--- haskell/staticblas/Level0.hs
+++ haskell/staticblas/Level0.hs
@@ -6,139 +6,123 @@ module Level0 where
 
 import GHC.TypeLits
 
--- Types v and s form a CoordinateSpace when (v n s) is a type for n-tuples of elements each, called
--- a coordinate, of type s, the scalar field. In this case, the vector type constructor v has kind
--- Nat -> * -> k, where k is the kind of the scalar type s.
--- For a CoordinateSpace, operations dim, coord and operator (!) are available for obtaining the
--- dimension of the vector space and the n'th coordinate of a vector.
-class CoordinateSpace v s | v -> s where
-    dim :: SingI n => v (n::Nat) s -> Int
+-- There is a CoordinateSpace involving types v, n and s when (v n s) is a type for n-tuples of
+-- elements each, called a coordinate, of type s, the scalar field, with n being a natural number
+-- type. In this case, the vector type constructor v has kind Nat -> * -> k, where k is the kind of
+-- the scalar type s.
+-- For a CoordinateSpace, operations dim, operator (!) and coordinate are available for obtaining
+-- the dimension of the vector space and the i'th coordinate of a vector.
+class SingI n => CoordinateSpace v n s | v -> n s where
+    infixl 9 !
+    (!) :: v n s -> Int -> s
+    dim :: v (n::Nat) s -> Int
     dim u = fromIntegral (withSing (toValue u))
         where toValue :: v (n::Nat) s -> Sing n -> Integer
               toValue _ n = fromSing n
-    coord :: Int -> v n s -> s
-    (!) :: SingI n => v n s -> Int -> s
-    (!) u i | i >= 0 && i < n = coord i u
-            | otherwise       = error "(!): out of bounds"
+    coordinate :: v n s -> Int -> s
+    coordinate u i | i >= 0 && i < n = u!i
+                   | otherwise       = error "coordinate: out of bounds"
         where n = dim u
 
--- Given a CoordinateSpace v s, if type s is equality comparable, then type (v n s) is also equality
--- comparable. Two vectors of type (v n s) are equal if all coordinates are equal.
-instance (CoordinateSpace v s, Eq s, SingI n) => Eq (v n s) where
-    u == v = allEqual u v 0
+-- Given a CoordinateSpace v n s, if type s is equality comparable, then type (v n s) is also
+-- equality comparable. Two vectors of type (v n s) are equal if all coordinates are equal.
+instance (CoordinateSpace v n s, Eq s) => Eq (v n s) where
+    u == v = allEqual 0
         where n = dim u
-              allEqual u v i | i < n     = (coord i u == coord i v) && allEqual u v (succ i)
-                             | otherwise = True
+              allEqual i | i < n     = (u!i == v!i) && allEqual (succ i)
+                         | otherwise = True
 
--- Given a CoordinateSpace v s, if type s is an instance of Show, then type (v n s) is also an
+-- Given a CoordinateSpace v n s, if type s is an instance of Show, then type (v n s) is also an
 -- instance of Show. Default method is to display all coordinates.
-instance (CoordinateSpace v s, Show s, SingI n) => Show (v n s) where
-    show u = "(" ++ (showInit u 0)
+instance (CoordinateSpace v n s, Show s) => Show (v n s) where
+    show u = '(' : showInit 0
         where n = dim u
-              step u i = show (coord i u) ++ showLoop u (succ i)
-              showInit u i | i < n     = step u i
-                           | otherwise = ")"
-              showLoop u i | i < n     = ' ' : step u i
-                           | otherwise = ")"
+              step i = show (u!i) ++ showLoop (succ i)
+              showInit i | i < n     = step i
+                         | otherwise = ")"
+              showLoop i | i < n     = ' ' : step i
+                         | otherwise = ")"
 
-class Matrix t s | t -> s where
-    rows :: (SingI m, SingI n) => t (m::Nat) (n::Nat) s -> Int
-    rows a = fromIntegral (withSing (toValue a))
-        where toValue :: t (m::Nat) (n::Nat) s -> Sing m -> Integer
-              toValue _ m = fromSing m
-    columns :: (SingI m, SingI n) => t (m::Nat) (n::Nat) s -> Int
-    columns a = fromIntegral (withSing (toValue a))
-        where toValue :: t (m::Nat) (n::Nat) s -> Sing n -> Integer
-              toValue _ n = fromSing n
-    --row :: (SingI m, SingI n, CoordinateSpace v s) => t m n s -> Int -> v n s
-    --column :: (SingI m, SingI n, CoordinateSpace v s) => t m n s -> Int -> v m s
-    --entry :: (SingI m, SingI n) => t m n s -> Int -> Int -> s
-    --entry a i j = (SingI m, SingI n) => t m n s -> Int -> Int -> s
-
--- Types v and s form a LinearSpace if both s and (v s) are instances of Num and scalar
--- multiplication is defined over s and (v s).
-class (Num s, Num (v s)) => LinearSpace v s | v -> s where
+-- There is a VectorSpace involving types v and s, if both (v s), the vector type and s, the scalar
+-- type, are instances of Num and scalar multiplication is defined over (v s) and s.
+class (Num s, Num (v s)) => VectorSpace v s | v -> s where
     scal :: s -> v s -> v s
     axpy :: s -> v s -> v s -> v s
 
--- Types v and s form a VectorSpace if an inner product if they define a LinearSpace and if an inner
--- product is defined over (v s) with an associated norm.
-class LinearSpace v s => VectorSpace v s where
+-- VectorSpace v s is additionally an InnerProductSpace if an inner product is defined over (v s),
+-- if s is an instance of Floating (with sqrt defined) and an associated norm is defined.
+class (VectorSpace v s, Floating s) => InnerProductSpace v s where
     dot :: v s -> v s -> s
     norm :: v s -> s
+    norm u = sqrt (dot u u)
+
+-- The concept of a Matrix involves four types, t, m, n and s. m and n are natural number types
+-- specifying the number of rows and columns of the matrix. s is the scalar type and (t m n s) is
+-- the matrix type. The matrix type constructor t has kind Nat -> Nat -> * -> k where k is the kind
+-- of the scalar type s.
+class (SingI m, SingI n) => Matrix t m n s | t -> m n s where
+    rows :: t (m::Nat) (n::Nat) s -> Int
+    rows a = fromIntegral (withSing (toValue a))
+        where toValue :: t (m::Nat) (n::Nat) s -> Sing m -> Integer
+              toValue _ m = fromSing m
+    columns :: t (m::Nat) (n::Nat) s -> Int
+    columns a = fromIntegral (withSing (toValue a))
+        where toValue :: t (m::Nat) (n::Nat) s -> Sing n -> Integer
+              toValue _ n = fromSing n
+    infixl 9 !!!
+    (!!!) :: t m n s -> (Int, Int) -> s
+    entry :: t m n s -> Int -> Int -> s
+    entry a i j | i >= 0 && i < m && j >= 0 && j < n = a!!!(i,j)
+                | otherwise                          = error "entry: out of bounds"
+        where m = rows a
+              n = columns a
+    transpose :: t m n s -> t n m s
+    --row :: t m n s -> Int -> t 1 n s
+    --column :: t m n s -> Int -> t m 1 s
+    --rowVector :: CoordinateSpace v n s => t m n s -> Int -> v n s
+    --columnVector :: CoordinateSpace v m s => t m n s -> Int -> v m s
+
+-- Given a Matrix t m n s, if type s is equality comparable, then type (t m n s) is also equality
+-- comparable. Two matrices of type (t m n s) are equal if all entries are equal.
+instance (Matrix t m n s, Eq s) => Eq (t m n s) where
+    a == b = allEqual 0 0
+        where m = rows a
+              n = columns a
+              allEqual i j | i < m && j < n = (a!!!(i,j) == b!!!(i,j)) && allEqual i (succ j)
+                           | i < m          = allEqual (succ i) 0
+                           | otherwise      = True
+
+-- Given a Matrix t m n s, if type s is an instance of Show, then type (t m n s) is also an instance
+-- of Show. Default method is to display all entries in row-major order.
+instance (Matrix t m n s, Show s) => Show (t m n s) where
+    show a | m >= 1 && n >= 1 = '[' : showCol 0 0
+           | otherwise        = "[]"
+        where m = rows a
+              n = columns a
+              step i j = show (a!!!(i,j)) ++ showCol i (succ j)
+              showRow i j | i < m     = ';' : showCol i 0
+                          | otherwise = "]"
+              showCol i j | j < n     = ' ' : step i j
+                          | otherwise = ' ' : showRow (succ i) 0
+
+instance (Matrix t 1 n s) => CoordinateSpace (t 1) n s where
+    a ! j = a !!! (1,j)
+
+-- Any CoordinateSpace vector can be seen as a one column Matrix.
+--instance (CoordinateSpace v n s) => Matrix
 
 -- One natural concept used to represent a Vector in a functional language is a function. A Vector
 -- may be a function which maps a bounded range of the natural numbers to a number field.
 newtype FVector (n::Nat) s = FVector (Int -> s)
 
-instance CoordinateSpace FVector s where
-    coord i (FVector f) = f i
-
---newtype FVector n f = FVector (Int -> f)
+instance SingI n => CoordinateSpace FVector n s where
+    (FVector f) ! i = f i
 
 -- Similarly, a Matrix may be a function of two natural arguments to a number field.
-newtype FMatrix r k = FMatrix (Int -> Int -> k)
+newtype FMatrix (m::Nat) (n::Nat) s = FMatrix (Int -> Int -> s)
 
--- Another, sometimes more practical way to represent a Vector is as a list of numbers.
-newtype LVector n k = LVector [k]
-
--- Matrices can also be represented as a list of numbers, with the elements laid out in what is
--- called row-major or column-major order.
-newtype LRowMajor r k = LRowMajor [k]
-newtype LColumnMajor r k = LColumnMajor [k]
-
-{-
-data GVector n field repr where
-    FV :: Int -> (Int -> field) -> GVector Int field (Int -> field)
-    LV :: [field] -> GVector Int field [field]
-
-instance Vector FVector where
-    (!) (FVector f) i = f i
-    vconst x = FVector (\ i -> x)
-        | i >= 0 && i < n = 
-            
-
-class MatricialProduct lhs rhs res | lhs rhs -> res where
-
-
-class Vector where
-
-instance 
-class (
-class Dim i where
-
-class Vector i k where
-    (!) :: v -> i -> k
-
-instance Vector (FVector) where
-    (!) (FVector f) i = f i
-    vconst x = FVector (\ i -> x)
-
-v :: Num a => Int -> a
-v i = [1,2,3,4,5,6] 
-
-data Num a => Vector a = Vector 
-newtype (Integral i, Num k) => FVector = FVector (i -> k)
-newtype (Integral i, Integral j, Num k) => FMatrix = FMatrix (i -> j -> k)
-
-class (Enum i, Num k) => Vector i k 
-class Vector
-data Vector n a =
-class (Eq a, Show a) => AddGroup a where
-    plus, minus :: a -> a -> a
-    neg         :: a -> a
-    scale       :: (Num k) => k -> a -> a
-    minus x y = plus x (neg y)
-
-class (AddGroup a m n, Integral m, Integral n) => Matrix a m n where
-    transpose   :: a m n -> a n m
-    prod        :: a m n -> a n p -> a m p
-    toNum       :: Num k => a 1 1 -> k
-
-class Matrix a n 1 => Vector a n where
-    dot                     :: Num k => a n -> a n -> k
-    norm1, norm2, normInf   :: Num k => a n -> k
-    dot u v = toNum (prod (transpose u) v)
--}
-
+instance (SingI m, SingI n, Num s) => Matrix FMatrix m n s where
+    (FMatrix f) !!! (i,j) = f i j
+    transpose (FMatrix f) = FMatrix g
+        where g i j = f j i
 
--- haskell/staticblas/test.hs
+++ haskell/staticblas/test.hs
@@ -1,5 +1,5 @@
 -- Miguel Ramos, 2013.
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds, OverlappingInstances #-}
 
 module Main (main) where
 
@@ -13,15 +13,29 @@ u2 = (FVector (\ i -> if (i == 2) then 1 else 0))::(FVector 3 Int)
 
 x = (FVector (\ i -> if (i < 3) then 1 else 0))::(FVector 3 Int)
 
+id3 = (FMatrix (\ i j -> if (i == j) then 1 else 0))::(FMatrix 3 3 Int)
+
+a = (FMatrix (\ i j -> 3*i + j))::(FMatrix 3 3 Int)
+b = (FMatrix (\ i j -> i + 3*j))::(FMatrix 3 3 Int)
+
+c = (FMatrix (\ i j -> if (i == j) then 1 else 0))::(FMatrix 3 2 Int)
+
 main = do
-    putStrLn $ show $ zero
-    putStrLn $ show $ ones
-    putStrLn $ show $ u0
-    putStrLn $ show $ u1
-    putStrLn $ show $ u2
-    putStrLn $ show $ x
+    putStrLn $ (show zero) ++ " of size " ++ (show $ dim zero)
+    putStrLn $ show ones
+    putStrLn $ show u0
+    putStrLn $ show u1
+    putStrLn $ show u2
+    putStrLn $ show x
     putStrLn $ show $ x == zero
     putStrLn $ show $ x == ones
     putStrLn $ show $ x == u0
     putStrLn $ show $ x == u1
     putStrLn $ show $ x == u2
+
+    putStrLn $ show id3
+    putStrLn $ show a
+    putStrLn $ show b
+    putStrLn $ show c
+    putStrLn $ show $ a == b
+    putStrLn $ show $ a == a
