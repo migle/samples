81c2ad24c9c079673770a14afcd91b3eadd8018c
Miguel Lopes Santos Ramos <mail@miguel.ramos.name>
2014-03-27 12:03:59 +0000

    Making accumulator parameters strict.

--- haskell/fibonacci/FiboList.hs
+++ haskell/fibonacci/FiboList.hs
@@ -1,7 +1,11 @@
--- Miguel Ramos, 2013.
+-- Miguel Ramos, 2013-2014.
 
 module FiboList (fib, fibs) where
 
-fib n = fibs !! n
+fib n = nth fibs n
+        where nth (x:_)  0 = x
+              nth (x:xs) n = x `seq` nth xs (n - 1)
+
+-- fib n = fibs !! n
 
 fibs = 0 : 1 : [ a + b | (a, b) <- zip fibs (tail fibs)]
--- haskell/fibonacci/FiboTail.hs
+++ haskell/fibonacci/FiboTail.hs
@@ -1,8 +1,8 @@
--- Miguel Ramos, 2013.
+-- Miguel Ramos, 2013-2014.
 
 module FiboTail (fib) where
 
 fib n = gfib n 0 1
 	where
 		gfib 0 a b = a
-		gfib n a b = gfib (n - 1) b (a + b)
+		gfib n a b = gfib (n - 1) b $! (a + b)
