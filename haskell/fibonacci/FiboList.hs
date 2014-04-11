64dbac9e7bce1dd387b828e2deb1a7256d86c90c
Miguel Lopes Santos Ramos <mail@miguel.ramos.name>
2014-04-11 14:13:19 +0100

    More strictness in FiboList.

--- haskell/fibonacci/FiboList.hs
+++ haskell/fibonacci/FiboList.hs
@@ -2,10 +2,11 @@
 
 module FiboList (fib, fibs) where
 
-fib n = nth fibs n
-        where nth (x:_)  0 = x
-              nth (x:xs) n = x `seq` nth xs (n - 1)
+fib n = fibs !! n
 
--- fib n = fibs !! n
+-- fibs = 0 : 1 : [ a + b | (a, b) <- zip fibs (tail fibs)]
 
-fibs = 0 : 1 : [ a + b | (a, b) <- zip fibs (tail fibs)]
+fibs = 0 : 1 : zipAdd fibs (tail fibs)
+       where zipAdd (a:as) (b:bs) = let c = a + b
+                                    in seq c $ c : zipAdd as bs
+             zipAdd _ _           = []
