23cc190e088b982c979ab041583320563839a042
Miguel Lopes Santos Ramos <mail@miguel.ramos.name>
2014-09-11 15:30:49 +0100

    More list possibilities: strict return and parallel list comprehensions.

--- haskell/fibonacci/FiboList.hs
+++ haskell/fibonacci/FiboList.hs
@@ -1,12 +1,22 @@
 -- Miguel Ramos, 2013-2014.
+{-# LANGUAGE ParallelListComp #-}
 
 module FiboList (fib, fibs) where
 
 fib n = fibs !! n
 
--- fibs = 0 : 1 : [ a + b | (a, b) <- zip fibs (tail fibs)]
+-- fibs = 0 : 1 : [ a + b | (a, b) <- zip fibs (tail fibs) ]
+-- fibs = 0 : 1 : [ a + b | a <- fibs | b <- tail fibs ]
+-- fibs = 0 : 1 : do
+--     (a, b) <- zip fibs (tail fibs)
+--     return $! a + b
+-- fibs = 0 : 1 : [ n | n <- zipWith (+) fibs (tail fibs) ]
+-- fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+fibs = 0 : 1 : do
+    n <- zipWith (+) fibs (tail fibs)
+    return $! n
 
-fibs = 0 : 1 : zipAdd fibs (tail fibs)
-       where zipAdd (a:as) (b:bs) = let c = a + b
-                                    in seq c $ c : zipAdd as bs
-             zipAdd _ _           = []
+-- fibs = 0 : 1 : zipAdd fibs (tail fibs)
+--     where zipAdd (a:as) (b:bs) = let c = a + b
+--                                  in c `seq` c : zipAdd as bs
+--           zipAdd _ _           = []
