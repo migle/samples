09d84d8cf2d7bd1b8a8843558e38182378773367
Miguel Lopes Santos Ramos <mail@miguel.ramos.name>
2014-09-11 14:14:17 +0100

    Optimized ST version.

--- haskell/fibonacci/FiboST.hs
+++ haskell/fibonacci/FiboST.hs
@@ -6,22 +6,27 @@ import Control.Monad
 import Control.Monad.ST
 import Data.STRef
 
-forward (a,b) = (b,a+b)
-backward (a,b) = (b-a,a)
-moveST n p | n > 0     = do
-                            modifySTRef p $! forward
-                            moveST (n - 1) p
-           | n < 0     = do
-                            modifySTRef p $! backward
-                            moveST (n + 1) p
-           | otherwise = readSTRef p
+forward (a,b) = let c = a + b in c `seq` (b,c)
+backward (a,b) = let c = b - a in c `seq` (c,a)
 
-fib = fib3
-
-fib1 n = fst $ runST $
+fib n = fst $ runST $
   do
     cursor <- newSTRef (0,1)
-    moveST n cursor
+    if n > 0 then
+      advance n cursor
+    else
+      retreat n cursor
+  where
+    advance 0 p = readSTRef p 
+    advance n p = do
+      p' <- readSTRef p
+      writeSTRef p $! forward p'
+      advance (n - 1) p
+    retreat 0 p = readSTRef p
+    retreat n p = do
+      p' <- readSTRef p
+      writeSTRef p $! backward p'
+      retreat (n + 1) p 
 
 fib2 n = fst $ runST $
   do
@@ -31,14 +36,14 @@ fib2 n = fst $ runST $
     readSTRef cursor
 
 fib3 n = runST $
-            do
-                a <- newSTRef 0
-                b <- newSTRef 1
-                fibST n a b
-         where fibST 0 a _ = readSTRef a
-               fibST n a b = do
-                    a' <- readSTRef a
-                    b' <- readSTRef b
-                    writeSTRef a b'
-                    writeSTRef b $! a'+b'
-                    fibST (n - 1) a b
+  do
+    a <- newSTRef 0
+    b <- newSTRef 1
+    fibST n a b
+  where fibST 0 a _ = readSTRef a
+        fibST n a b = do
+            a' <- readSTRef a
+            b' <- readSTRef b
+            writeSTRef a b'
+            writeSTRef b $! a'+b'
+            fibST (n - 1) a b
