d87dc34146b299c06cf68fbb5789a70cc9d3d258
Miguel Lopes Santos Ramos <mail@miguel.ramos.name>
2014-10-09 11:46:20 +0100

    Neater version.

--- haskell/mini/pairs.hs
+++ haskell/mini/pairs.hs
@@ -4,10 +4,8 @@ import System.Environment (getArgs)
 
 pairs n = [ (i, j) | i <- [ 1 .. n ], j <- [ (i + 1) .. n ] ]
 
-add (a, b) = a + b
-
-nPairs n = length $ pairs n
-sumPairs n = sum $ map add (pairs n)
+nPairs = length . pairs
+sumPairs = sum . map (\ (a, b) -> a + b) . pairs
 
 main = do
   args <- getArgs
