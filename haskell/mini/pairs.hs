aa90ef0a15966b5b31219425f09f3bddb4fc0201
Miguel Lopes Santos Ramos <mail@miguel.ramos.name>
2014-10-30 13:57:10 +0000

    Version with my own sum function.

--- haskell/mini/pairs.hs
+++ haskell/mini/pairs.hs
@@ -1,17 +1,40 @@
 -- Miguel Ramos, 2014.
+
+-- We use two language extensions mostly so that we can write Haskell code which is a pretty direct
+-- translation from lower-level STG code.
+-- BangPatters allows us to force function parameters to be strict.
+-- MagicHash is required so we can use the '#' character, as in GHC.Prim.Int#.
 {-# LANGUAGE BangPatterns, MagicHash #-}
 
-import GHC.Exts
-import GHC.Prim
+import GHC.Exts -- From this we use I# which is the data constructor to box and unbox Ints.
+import GHC.Prim -- From this we use the primitive type GHC.Prim.Int# and its operators.
+
 import System.Environment (getArgs)
 
+-- The type declarations below have the sole purpose of strengthening the type of expressions to
+-- Int so that the compiler output is more readable.
+
+-- Compute the pairs of Ints from 1 up to n.
 pairs :: Int -> [(Int, Int)]
 pairs n = [ (i, j) | i <- [ 1 .. n ], j <- [ (i + 1) .. n ] ]
 
+-- Compute the sums of pairs of Ints from 1 up to n.
 sums :: Int -> [Int]
 sums n = [ i + j | i <- [ 1 .. n ], j <- [ (i + 1) .. n ] ]
 
+-- Compute the sum of a list of pairs of Ints (notice currying).
+sumSum :: [(Int, Int)] -> Int
 sumSum = sum . map (\ (a, b) -> a + b)
+
+-- Compute the sum of a list of Ints.
+mySum :: [Int] -> Int
+mySum xs = acc 0 xs
+  where acc !z []     = z
+        acc !z (x:xs) = acc (z + x) xs
+
+-- These are the variations of the program used by the main function.
+-- All are NOINLINE so that we can read the compiler output for each of them separately.
+-- Notice that all of these functions are curried.
 
 {-# NOINLINE base #-}
 base = pairs
@@ -19,8 +42,10 @@ base = pairs
 baseN = length . pairs
 {-# NOINLINE baseSum #-}
 baseSum = sumSum . pairs
-{-# NOINLINE baseSums #-}
-baseSums = sum . sums
+{-# NOINLINE baseSumSums #-}
+baseSumSums = sum . sums
+{-# NOINLINE baseMySumSums #-}
+baseMySumSums = mySum . sums
 
 {-# NOINLINE stg #-}
 stg = stgPairs
@@ -28,7 +53,10 @@ stg = stgPairs
 stgN = length . stgPairs
 {-# NOINLINE stgSum #-}
 stgSum = sumSum . stgPairs
-{-# NOINLINE stgSums #-}
+{-# NOINLINE stgSumSums #-}
+stgSumSums = sum . stgSums
+{-# NOINLINE stgMySumSums #-}
+stgMySumSums = mySum . stgSums
 
 {-# NOINLINE re #-}
 re = rePairs
@@ -36,8 +64,13 @@ re = rePairs
 reN = length . rePairs
 {-# NOINLINE reSum #-}
 reSum = sumSum . rePairs
-{-# NOINLINE reSums #-}
+{-# NOINLINE reSumSums #-}
+reSumSums = sum . reSums
+{-# NOINLINE reMySumSums #-}
+reMySumSums = mySum . reSums
 
+
+-- This is the main function.
 main = do
   args <- getArgs
   case args of
@@ -48,7 +81,9 @@ main = do
     [ "sum", arg ]
         | n <- read arg -> print $ baseSum n
     [ "sums", arg ]
-        | n <- read arg -> print $ baseSums n
+        | n <- read arg -> print $ baseSumSums n
+    [ "mysums", arg ]
+        | n <- read arg -> print $ baseMySumSums n
     [ "stg-pr", arg ]
         | n <- read arg -> print $ stg n
     [ "stg-n", arg ]
@@ -56,7 +91,9 @@ main = do
     [ "stg-sum", arg ]
         | n <- read arg -> print $ stgSum n
     [ "stg-sums", arg ]
-        | n <- read arg -> print $ stgSums n
+        | n <- read arg -> print $ stgSumSums n
+    [ "stg-mysums", arg ]
+        | n <- read arg -> print $ stgMySumSums n
     [ "re-pr", arg ]
         | n <- read arg -> print $ re n
     [ "re-n", arg ]
@@ -64,7 +101,9 @@ main = do
     [ "re-sum", arg ]
         | n <- read arg -> print $ reSum n
     [ "re-sums", arg ]
-        | n <- read arg -> print $ reSums n
+        | n <- read arg -> print $ reSumSums n
+    [ "re-mysums", arg ]
+        | n <- read arg -> print $ reMySumSums n
 
 
 -- This is how pairs looks when compiled to STG.
@@ -112,103 +151,91 @@ stgPairs' n =
                                       in g jFirst
          in f 1#
 
--- We can simplify the STG for presentation, mostly eliminating boxed/unboxed complexity.
+-- We can simplify the STG for presentation, mostly eliminating boxing/unboxing complexity.
 rePairs :: Int -> [(Int, Int)]
 rePairs n = case 1 > n of
               True -> []
-              False -> let f i = case i + 1 of
-                                   jFirst@_ ->
-                                     let fTail = case i == n of
-                                                   True -> []
-                                                   False -> case i + 1 of
-                                                              iSucc@_ -> f iSucc
-                                     in case jFirst > n of
-                                          True -> fTail
-                                          False -> let g j = let gTail = case j == n of
-                                                                           True -> fTail
-                                                                           False -> case j + 1 of
-                                                                                      jSucc@_ -> g jSucc
-                                                             in let pair = (i, j)
-                                                                in pair : gTail
-                                                   in g jFirst
-                       in f 1
+              False ->
+                let f !i = case i + 1 of
+                             jFirst@_ ->
+                               let fTail = case i == n of
+                                             True -> []
+                                             False -> case i + 1 of
+                                                        iSucc@_ -> f iSucc
+                               in case jFirst > n of
+                                    True -> fTail
+                                    False ->
+                                      let g !j = let gTail = case j == n of
+                                                               True -> fTail
+                                                               False -> case j + 1 of
+                                                                          jSucc@_ -> g jSucc
+                                                 in let pair = (i, j)
+                                                    in pair : gTail
+                                      in g jFirst
+                in f 1
 
--- Same thing using patterns and "where", instead of "case" or "if-then-else", which looks nicer.
-prPairs :: Int -> [(Int, Int)]
-prPairs n | 1 > n     = []
-          | otherwise = let f i | jFirst > n = fTail
-                                | otherwise  = let g j = (i, j) : gTail
-                                                     where gTail | j == n    = fTail
-                                                                 | otherwise = g (j + 1)
-                                               in g jFirst
-                              where jFirst = i + 1
-                                    fTail | i == n    = []
-                                          | otherwise = f (i + 1)
-                        in f 1
+-- And this is how sums looks when compiled to STG, it is almost the same as pairs.
+stgSums :: Int -> [Int]
+stgSums n = case n of
+              I# i -> stgSums' i
 
--- This is how sums looks when compiled to STG.
-sumZero :: Int
-sumZero = sum []
+stgSums' :: Int# -> [Int]
+stgSums' n =
+  case 1# ># n of
+    nlt1@_ ->
+      case (tagToEnum# nlt1)::Bool of
+        True -> []
+        False ->
+          let f :: Int# -> [Int]
+              f i = case i +# 1# of
+                      jFirst@_ ->
+                        let fTail :: [Int]
+                            fTail = case i ==# n of
+                                      ieqn@_ ->
+                                        case (tagToEnum# ieqn)::Bool of
+                                          True -> []
+                                          False -> case i +# 1# of
+                                                     iSucc@_ -> f iSucc
+                        in case jFirst ># n of
+                             jgtn@_ ->
+                               case (tagToEnum# jgtn)::Bool of
+                                 True -> fTail
+                                 False ->
+                                   let g :: Int# -> [Int]
+                                       g j = let gTail :: [Int]
+                                                 gTail = case j ==# n of
+                                                           jeqn@_ ->
+                                                             case (tagToEnum# jeqn)::Bool of
+                                                               True -> fTail
+                                                               False -> case j +# 1# of
+                                                                          jSucc@_ -> g jSucc
+                                             in case i +# j of
+                                                  ipj@_ -> let box :: Int
+                                                               box = I# ipj
+                                                           in box : gTail
+                                   in g jFirst
+          in f 1#
 
-stgSums :: Int -> Int
-stgSums x = case x of
-              I# n ->
-                case 1# ># n of
-                  nlt1@_ ->
-                    case (tagToEnum# nlt1)::Bool of
-                      True -> sumZero
-                      False ->
-                        let f :: Int# -> [Int]
-                            f i = case i +# 1# of
-                                    jFirst@_ ->
-                                      let fTail :: [Int]
-                                          fTail = case i ==# n of
-                                                    ieqn@_ ->
-                                                      case (tagToEnum# ieqn)::Bool of
-                                                        True -> []
-                                                        False -> case i +# 1# of
-                                                                   iSucc@_ -> f iSucc
-                                      in case jFirst ># n of
-                                           jgtn@_ ->
-                                             case (tagToEnum# jgtn)::Bool of
-                                               True -> fTail
-                                               False ->
-                                                 let g :: Int# -> [Int]
-                                                     g j = let gTail :: [Int]
-                                                               gTail = case j ==# n of
-                                                                         jeqn@_ ->
-                                                                           case (tagToEnum# jeqn)::Bool of
-                                                                             True -> fTail
-                                                                             False -> case j +# 1# of
-                                                                                        jSucc@_ -> g jSucc
-                                                           in case i +# j of
-                                                                ipj@_ ->
-                                                                  let box :: Int
-                                                                      box = I# ipj
-                                                                  in box : gTail
-                                                 in g jFirst
-                        in case f 1# of
-                             list@_ -> sum list
-
--- Again, this is the STG simplified eliminating boxed/unboxed stuff.
-reSums :: Int -> Int
+-- Again, this is the STG simplified eliminating boxing/unboxing stuff.
+reSums :: Int -> [Int]
 reSums n = case 1 > n of
-             True -> sumZero
-             False -> let f i = case i + 1 of
-                                  jFirst@_ ->
-                                    let fTail = case i == n of
-                                                  True -> []
-                                                  False -> case i + 1 of
-                                                             iSucc@_ -> f iSucc
-                                    in case jFirst > n of
-                                         True -> fTail
-                                         False -> let g j = let gTail = case j == n of
-                                                                          True -> fTail
-                                                                          False -> case j + 1 of
-                                                                                     jSucc@_ -> g jSucc
-                                                            in case i + j of
-                                                                 ipj@_ -> ipj : gTail
-                                                  in g jFirst
-                      in case f 1 of
-                           list@_ -> sum list
+             True -> []
+             False ->
+               let f !i = case i + 1 of
+                            jFirst@_ ->
+                              let fTail = case i == n of
+                                            True -> []
+                                            False -> case i + 1 of
+                                                       iSucc@_ -> f iSucc
+                              in case jFirst > n of
+                                   True -> fTail
+                                   False ->
+                                     let g !j = let gTail = case j == n of
+                                                              True -> fTail
+                                                              False -> case j + 1 of
+                                                                         jSucc@_ -> g jSucc
+                                                in case i + j of
+                                                     ipj@_ -> ipj : gTail
+                                     in g jFirst
+               in f 1
 
